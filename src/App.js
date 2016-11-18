35aa1b39d323b64c7c30b816bafbc589de5d9606
Hiroyoshi Takahashi <t.hiroyoshi209@gmail.com>
2016-11-19 03:57:33 +0900

    Add news sction

--- src/App.js
+++ src/App.js
@@ -13,19 +13,27 @@ class App extends Component {
             </nav>
             <nav className="navbar-right">
               <ul className="list list-unstyled list-inline">
-                <li><a href="#section1">hoge</a></li>
-                <li><a href="#mission">mission</a></li>
-                <li><a href="#about">about</a></li>
+                <li>
+                  <a href="#mission">mission</a>
+                </li>
+                <li>
+                  <a href="#members">members</a>
+                </li>
+                <li>
+                  <a href="#news">news</a>
+                </li>
+                <li>
+                  <a href="#about">about</a>
+                </li>
               </ul>
             </nav>
           </div>
         </header>
-        <div className="App-body">
-          <section id="section1">
-            <h1>HOGEEEEEEEEEEEEEEEEEEEEEEEEEEe</h1>
-          </section>
+        <div className="App-body container">
           <section id="mission">
-            <h1>日本に、世界に新しい文化を。</h1>
+            <h1>
+              日本に、世界に新しい文化を。
+            </h1>
             <p>
               株式会社Shinonomeは、文化を作るために活動している小さな会社です。
             </p>
@@ -48,8 +56,73 @@ class App extends Component {
               Shinonomeは、挑戦する人を全力で応援します。
             </p>
           </section>
+          <section id="members">
+            <div className="row">
+              <div className="col-xs-4">
+                <div className="Member-panel">
+                  <div className="Member-panel-body">
+                    <img src="/hangai.png" className="Member-panel-body-photo img-circle" alt="hangai" />
+                    <h3 className="Member-panel-body-title">
+                      Seiichiro Hangai
+                    </h3>
+                    <p className="Member-panel-body-description">
+                      Outside Director
+                    </p>
+                  </div>
+                  <div className="Member-panel-footer">
+                    <p className="Member-panel-footer-description text-left">
+                      1975年東京理科大学工学部電気工学科卒，1981年同大学大学院理工学研究科博士課程了，工学博士。
+                      現在、東京理科大学理事ならびに工学部電気工学科教授。
+                      1996年～97年に在外研究員としてスタンフォード大学に滞在、2010年～2014年に工学部長。
+                      生体認証に関わるISO/IEC JTC1 SC37データフォーマットの国際標準化に貢献。
+                      文部科学省、人事院などの委員を歴任。
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-xs-4"></div>
+              <div className="col-xs-4"></div>
+            </div>
+            <div className="row">
+              <div className="col-xs-4"></div>
+              <div className="col-xs-4"></div>
+              <div className="col-xs-4"></div>
+            </div>
+          </section>
+          <sections id="news">
+            <div className="News-header">
+              <h1 className="News-header-title text-center">NEWS</h1>
+            </div>
+            <div className="News-body">
+              <ul className="News-body-list list-unstyled">
+                <li className="News-body-list-item">
+                  <div className="row">
+                    <div className="col-xs-3">
+                      <img src="/hangai.png" className="News-body-list-item-icon img-rounded" alt="newes" />
+                    </div>
+                    <div className="col-xs-9">
+                      <h4 className="text-left News-body-list-item-date">
+                        date
+                      </h4>
+                      <p className="text-left News-body-list-item-description">
+                        hogehogehogeohgoehgoehgoeogheohgoehgoe
+                        hogehogehogeohgoehgoehgoeogheohgoehgoe
+                        hogehogehogeohgoehgoehgoeogheohgoehgoe
+                        gehoghoehgoehogc
+                      </p>
+                      <a href="#" className="btn btn-danger pull-right">
+                        check
+                      </a>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </sections>
           <section id="about">
-            <h1>企業概要</h1>
+            <h1>
+              企業概要
+            </h1>
             <p>
               社名: 株式会社Shinonome (Shinonome inc.)
               代表者名: 種市隼兵
