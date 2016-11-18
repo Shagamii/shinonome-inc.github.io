d227687877b98bf2d5a343bcdb0390f79bcfa408
Hiroyoshi Takahashi <t.hiroyoshi209@gmail.com>
2016-11-19 02:36:23 +0900

    Add section structure

--- src/App.js
+++ src/App.js
@@ -6,16 +6,33 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
+        <header className="App-header">
+          <div className="container">
+            <nav className="navbar-left">
+              <img src={logo} className="App-logo" alt="logo" />
+            </nav>
+            <nav className="navbar-right">
+              <ul className="list list-unstyled list-inline">
+                <li><a href="#section1">hoge</a></li>
+                <li><a href="#section2">hoge</a></li>
+                <li><a href="#section3">hoge</a></li>
+              </ul>
+            </nav>
+          </div>
+        </header>
         <div className="App-body">
+          <section id="section1">
+          </section>
+          <section id="section2">
+          </section>
+          <section id="section3">
+          </section>
         </div>
-        <div className="App-footer">
+        <footer className="App-footer">
           <strong>
             &copy; 2016 Shinonome inc. All rights reserved
           </strong>
-        </div>
+        </footer>
       </div>
     );
   }
